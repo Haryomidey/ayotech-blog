c56dd73f87aae4ebbe770879a59c8422b6faba56
haryomidey <oladiipoayomide2021@gmail.com>
2024-08-04 14:01:43 +0100

    latest update

--- backend/configs/keys.js
+++ backend/configs/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbURI: 'mongodb+srv://ayomide:ayomide10@cluster0.vppn0rm.mongodb.net/?retryWrites=true&w=majority',
+    dbURI: "mongodb+srv://ayotech:ayotech@ayotech.0vyuwic.mongodb.net/?retryWrites=true&w=majority&appName=Ayotech",
     secretKey: 'efjefjnfrnkfou9tu5rjnrfnktnk5ntkrnrj5tk5t',
     adminSecretKey: 'efjt45tijfofri04i54nktk34tnk34ntk4n3tk34kt45wt34t45'
 }
